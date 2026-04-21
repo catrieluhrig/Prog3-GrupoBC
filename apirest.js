282e1c4a3647eb84c85f638144bdd7135bc95a37
catrieluhrig <catrieluhrigg@gmail.com>
2026-04-21 17:10:54 -0300

    request GET basica con express

--- apirest.js
+++ apirest.js
@@ -1,2 +1,14 @@
 //Grupo BC
 //Integrantes: Sieza Sergio, Morabito Flavia, Guerrero Daiana, Revollo Federico, Uhrig Catriel.
+
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log("testing")
+})
+
+app.listen("3000", () => {
+    "Servidor iniciado en puerto 3000"
+})
