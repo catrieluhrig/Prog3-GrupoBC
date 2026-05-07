c9b8563f3cc7cdcb430bfe5b096d5864f3b90ff2
catrieluhrig <catrieluhrigg@gmail.com>
2026-05-07 14:57:02 -0300

    Instalación de express-validator, adición de metodo post para el BREAD

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 //Integrantes: Sieza Sergio, Morabito Flavia, Guerrero Daiana, Revollo Federico, Uhrig Catriel.
 
 import express from "express";
+import { pool } from "./DB_TEST/conexion-sql.js"
 import { testDB } from "./DB_TEST/test_db.js"
 
 const app = express();
+app.use(express.json());
 
 await testDB();
 
@@ -14,6 +16,22 @@ app.get("/", (req, res) => {
         "status": "HTTP 200 OK",
         "msg": "request completada"
     })
+})
+
+app.post("/especialidades", async (req, res) => {
+    try{
+        const { nombre } = req.body
+        const query = "INSERT INTO especialidades (nombre) VALUES (?)"
+        const [result] = await pool.execute(query, [nombre]) //El segundo parametro de execute() debe ser un array
+        res.status(201).send({
+            "status": "HTTP 201",
+            "msg": "Especialidad insertada correctamente"
+        })
+    } 
+    catch(error){
+        console.log("Error de red al insertar especialidad: ", error)
+    }
+    
 })
 
 process.loadEnvFile()
